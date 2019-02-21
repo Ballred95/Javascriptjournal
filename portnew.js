c857ee67da367e351ed28b512d93ffdd57c0fbb3
Blake <blake.allred95@icloud.com>
2019-02-21 10:33:06 -0700

    initial commit

---
+++ portnew.js
@@ -0,0 +1,38 @@
+//variables-containers that store values
+
+var myVariable // declared, but not initialized..it has no value
+// also, don't use "var" anymore. ES6 teaches us about "let and "const" 
+
+let thisVariable = 'blake' // declared and initialized
+
+const ANSWER = 42 // unchangeable 
+
+//STRING
+
+let string1 = 'Hello World'
+
+let string2 = new String('Hello world') // look up difference between string literals and using a constructor. 
+
+//NUMBER    
+
+let thisNumb = 3.14159
+
+//Boolean
+
+let thisBool = true
+
+let minArray = ['value'] //Array uses square brackets
+
+let my Array = [42, 'bob', thisBool, false] // array hold most anything
+
+// Object
+
+let minimalObject = {}
+
+const myCar = {make: 'dodge',
+color: 'grey',
+year: 2015,
+vin:1234567890,
+}
+
+
